d8c0f1d1f43e894f38a12e53e82319a955ae4901
André Filipe <andrefilipefernandes2002@gmail.com>
2025-03-20 00:19:55 +0000

    Small tests with buttons made

--- SCADA/SCADA_Project/Config/Table.ml
+++ SCADA/SCADA_Project/Config/Table.ml
@@ -3,3 +3,27 @@ Language0§Language1
 Eventos§Eventos
 Linha de Produção§Linha de Produção
 Alarmes§Alarmes
+Automático§Automático
+Manuntenção§Manuntenção
+Manual§Manual
+Tool 1§Tool 1
+Tool 2§Tool 2
+Tool 3§Tool 3
+Operar§Operar
+Ligar Tapete§Ligar Tapete
+%s§%s
+Date: %s§Date: %s
+Time: %s§Time: %s
+Date:§Date:
+Time:§Time:
+Button§Button
+Active§Active
+Ack§Ack
+Message§Message
+Date§Date
+Time§Time
+Class 1§Class 1
+Class 2§Class 2
+To ack§To ack
+Excluded§Excluded
+Exit§Exit
