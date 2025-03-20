06251f6c881ebed8157a8391dd84f382d31c0c58
André Filipe <andrefilipefernandes2002@gmail.com>
2025-03-20 12:49:16 +0000

    Changed switches to buttons Added some templates (Histórico, Gráficos)

--- SCADA/SCADA_Project/Config/Table.ml
+++ SCADA/SCADA_Project/Config/Table.ml
@@ -27,3 +27,18 @@ Class 2§Class 2
 To ack§To ack
 Excluded§Excluded
 Exit§Exit
+EXISTEM ALARMES§EXISTEM ALARMES
+Histórico§Histórico
+Gráficos§Gráficos
+Start date§Start date
+Start time§Start time
+End date§End date
+End time§End time
+Exit Program§Exit Program
+T1§T1
+T2§T2
+T3§T3
+Data:§Data:
+Hora:§Hora:
+T4§T4
+Painel de Alarmes§Painel de Alarmes
